2c87e90ab7e3ae396dc99dbc45ac315efbf47fce
exlineo <contact@exlineo.com>
2019-07-28 22:55:22 +0200

    Et hop, premier niveau en action

--- js/lolchat.js
+++ js/lolchat.js
@@ -1,18 +1,20 @@
-
-
 class LolChat {
     /**
      * 
      * @param {string} a Adresse du serveur Web Socket
      * @param {string} p Port de connexion
      * @param {HtmlElement} el Balise Html dans laquelle écrire le chat
+     * @param {HtmlElement} i Balise Html dans laquelle écrire les infos
      */
-    constructor(a, p, el) {
+    constructor(a, p, el, i) {
         this.adr = a;
         this.port = p;
-        this.socket = new io();
+        this.socket = io.connect(a, {transports: ['websocket', 'polling', 'flashsocket']});
+
+        this.id;
 
         this.el = el;
+        this.infos = i;
 
         this.creerEl();
         this.init();
@@ -24,26 +26,32 @@ class LolChat {
         this.socket.on('connect',
             () => {
                 console.log(this.socket.id);
-                this.ecritMessage(this.socket.id + ' id reçu');
+                this.ecritInfos(this.socket.id + ' id reçu');
+                this.id = this.socket.id;
             });
 
         this.socket.on('connexion',
             (data) => {
                 console.log(data);
-                this.ecriteMessage('connexion établie');
-                // socket.emit('my other event', { my: 'data' });
+                this.ecritInfos('connexion établie');
             });
 
-        this.socket.on('message',
+        this.socket.on('messageRecu',
             data => {
                 console.log(data);
-                this.ecriteMessage(data);
+                this.ecriteMessageEl(data);
+            });
+
+        this.socket.on('changementDeSalon',
+            data => {
+                console.log(data);
+                this.ecritInfos('Changement de salon : ', data);
             })
 
         this.socket.on('disconnect',
             () => {
                 this.socket.open();
-                this.ecriteMessage('Reconnexion');
+                this.ecriteMessageEl('Reconnexion');
         });
     }
     /**
@@ -60,6 +68,10 @@ class LolChat {
             this.changeSalon(ev.currentTarget.value);
         });
 
+        this.txt = document.createElement('input');
+        this.txt.name = 'Message';
+        this.txt.placeholder = 'Saisissez un message';
+
         for(let s of tab){
             let o = document.createElement('option');
             o.textContent = s;
@@ -69,6 +81,15 @@ class LolChat {
         
         this.el.appendChild(this.salons);
         this.el.appendChild(this.chat);
+        this.el.appendChild(this.txt);
+
+        document.addEventListener('keydown', (ev) => {
+            if(ev.key == 'Enter' && this.txt.value != ''){
+                this.message = this.txt.value;
+                this.socket.emit('messageEnvoye', this.message);
+                this.txt.value = '';
+            }
+        });
     }
     /**
      * Envoyer un événement au serveur pour indiquer le changement de salon
@@ -81,11 +102,23 @@ class LolChat {
      * Message reçu depuis le serveur
      * @param {string} m Message reçu depuis le serveur à écrire dans le chat
      */
-    ecriteMessage(m) {
+    ecriteMessageEl(m) {
         let li = document.createElement('li');
+        if(m == this.message){
+            li.className = 'self';
+        }
         li.textContent = m;
         this.chat.appendChild(li);
     }
+    /**
+     * 
+     * @param {sting} m Message à écrire en information 
+     */
+    ecritInfos(m){
+        let i = document.createElement("p");
+        i.textContent = m;
+        this.infos.appendChild(i);
+    }
 }
 
-var loool = new LolChat("http://localhost", '3200', document.querySelector('#chat'));
+var loool = new LolChat("http://localhost:3200", '3200', document.querySelector('#chat'), document.querySelector('#infos'));
